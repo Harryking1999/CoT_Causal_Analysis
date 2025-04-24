ef4d8750b8d615039472d14b64ff2ce95e0d9568
Harryking1999 <1823114030@qq.com>
2025-04-24 22:07:33 +0800

    fix bug

--- scripts/api_run.py
+++ scripts/api_run.py
@@ -133,6 +133,8 @@ def extract_answer(output, item, dataset, interfere_mode=0):
         output = output.split("</think>")[0].split(".")[0]
     elif interfere_mode == 2:
         output = output.split(".")[0]
+    # elif interfere_mode == 3:
+    #     output = output.split(".")[0]
     try:
         dataset = dataset.split(':')[0]
         if dataset in ['Addition', 'Product', 'GSM8K']:
