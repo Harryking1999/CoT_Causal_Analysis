ccded7b7398436f1a8dd4f2667da0a6440cd75b2
Delusion4013 <chenkai00cs@outlook.com>
2025-05-28 22:37:04 -0400

    implemented replace_other_CoT feature

---
+++ replace_other_CoT.py
@@ -0,0 +1,34 @@
+import json
+import argparse
+
+def process_files(file1_name):
+
+    file1_path = './exp_cot/output/' + file1_name
+
+
+    with open(file1_path, 'r') as f:
+        data = json.load(f)
+
+    replace_field = 'cot0shot.randomreason_input_assistant'
+
+    # Circular shift of 'field_name'
+    field_values = [d[replace_field] for d in data]
+    results = [d['answer'] for d in data]
+    for i in range(len(data)):
+        data[i]['cot0shot.randomreason_othercot_answer'] = results[i - 1]
+        data[i][replace_field] = field_values[i - 1]  # previous item's value (circular)
+
+    output_file = f"{file1_name}.replaced.json"
+    output_path = './exp_cot/output/' + output_file
+
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, required=True, help='input JSON file (will have its reason field replaced)')
+    args = parser.parse_args()
+    
+    # Process files
+    process_files(args.file) 
