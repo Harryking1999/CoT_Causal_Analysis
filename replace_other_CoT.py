05c7184ff6cd24a62fe5f903af4287f25c4ed855
Delusion4013 <chenkai00cs@outlook.com>
2025-05-28 23:45:21 -0400

    implemented replace_other_cot feature with experiments

--- replace_other_CoT.py
+++ replace_other_CoT.py
@@ -9,19 +9,20 @@ def process_files(file1_name):
     with open(file1_path, 'r') as f:
         data = json.load(f)
 
-    replace_field = 'cot0shot.randomreason_input_assistant'
-
+    replace_field = 'cot0shot.math teacher_thinking_CoT'
+    new_field = 'cot0shot.math teacher_othercot'
     # Circular shift of 'field_name'
     field_values = [d[replace_field] for d in data]
     results = [d['answer'] for d in data]
+
     for i in range(len(data)):
         data[i]['cot0shot.randomreason_othercot_answer'] = results[i - 1]
-        data[i][replace_field] = field_values[i - 1]  # previous item's value (circular)
+        data[i][new_field] = field_values[i - 1]  # previous item's value (circular)
 
-    output_file = f"{file1_name}.replaced.json"
-    output_path = './exp_cot/output/' + output_file
+    # output_file = f"{file1_name}.replaced.json"
+    # output_path = './exp_cot/output/' + output_file
 
-    with open(output_path, 'w') as f:
+    with open(file1_path, 'w') as f:
         json.dump(data, f, indent=2)
 
 
