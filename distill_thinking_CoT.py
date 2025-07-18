650ceef7a2fb6d2cda6d7964385bcb96ce8ae2ff
Harryking1999 <fuzhizhang.fzz@gmail.com>
2025-07-18 21:09:01 +0800

    add file

--- distill_thinking_CoT.py
+++ distill_thinking_CoT.py
@@ -183,8 +183,10 @@ def process_file(input_file):
     key_prefix = None
     if('newdirectbase' in input_file):
         key_prefix = 'newdirectbase'
-    else:
+    elif('newdirect' in input_file):
         key_prefix = 'newdirect'
+    else:
+        key_prefix = 'cot0shot'
 
     flag_openr1_pattern = False
     if(".Qwen2.5-3B-GRPO" in input_file or ".Qwen2.5-3B-Distill-GRPO" in input_file):
