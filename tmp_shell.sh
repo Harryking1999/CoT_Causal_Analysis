1cfa4eceb05aa7ffc532189a25e2a9eb0622c70f
Harryking1999 <1823114030@qq.com>
2025-07-21 09:59:38 +0800

    add file

--- tmp_shell.sh
+++ tmp_shell.sh
@@ -1,6 +1,7 @@
-sh random_reason.sh DeepSeekR1-Qwen-32B ProofWriter newdirect
-sh random_reason.sh DeepSeekR1-Qwen-32B LOGIQA newdirect
-sh random_reason.sh DeepSeekR1-Qwen-32B FOLIO newdirect
+sh random_reason.sh Qwen2.5-3B-GRPO-4000 ProofWriter newdirectbase
+sh random_reason.sh Qwen2.5-3B-GRPO-4000 LOGIQA newdirectbase
+sh random_reason.sh Qwen2.5-3B-GRPO-4000 FOLIO newdirectbase
+python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path ./exp_cot/output/output.MATH500.newdirectbase.math_teacher.Qwen2.5-3B-GRPO-4000.json --mode 1
 # python .\scripts\openai_math500.py --model_name gpt-4.1-mini --file_path ./exp_cot/output/output.MATH500.newdirect.math_teacher.deepseek-reasoner.json --mode 1
 # python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path ./exp_cot/output/output.MATH500.newdirect.math_teacher.QwQ-32B.json --mode 1
 # python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path ./exp_cot/output/output.MATH500.newdirect.math_teacher.DeepSeekR1-Qwen-7B.json --mode 1
