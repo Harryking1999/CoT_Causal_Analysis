71f857afea8986877f3e43bfc3a3ba69fff8ddba
Harryking1999 <fuzhizhang.fzz@gmail.com>
2025-07-29 10:58:36 +0800

    add file

--- distill_thinking_CoT.py
+++ distill_thinking_CoT.py
@@ -189,10 +189,10 @@ def process_file(input_file):
         key_prefix = 'cot0shot'
 
     flag_openr1_pattern = False
-    if(".Qwen2.5-3B-GRPO" in input_file or ".Qwen2.5-3B-SFT-GRPO" in input_file):
-        flag_openr1_pattern = True
-    if("Qwen2.5-3B-SFT-GRPO-200." in input_file or "Qwen2.5-3B-SFT-GRPO-400." in input_file):
-        flag_openr1_pattern = False
+    # if(".Qwen2.5-3B-GRPO" in input_file or ".Qwen2.5-3B-SFT-GRPO" in input_file):
+    #     flag_openr1_pattern = True
+    # if("Qwen2.5-3B-SFT-GRPO-200." in input_file or "Qwen2.5-3B-SFT-GRPO-400." in input_file):
+    #     flag_openr1_pattern = False
     
     # Extract dataset name from filename
     # print(items[0:2])
