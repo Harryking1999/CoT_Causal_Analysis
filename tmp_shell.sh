346209f051e13b987e64db8457f1f90226eb8d89
Harryking1999 <1823114030@qq.com>
2025-07-31 10:48:31 +0800

    add file

--- tmp_shell.sh
+++ tmp_shell.sh
@@ -1,7 +1,7 @@
-sh random_reason.sh Qwen2.5-3B-GRPO-600 ProofWriter newdirectbase
-sh random_reason.sh Qwen2.5-3B-GRPO-600 LOGIQA newdirectbase
-sh random_reason.sh Qwen2.5-3B-GRPO-600 FOLIO newdirectbase
-python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path ./exp_cot/output/output.MATH500.newdirectbase.math_teacher.Qwen2.5-3B-GRPO-600.json --mode 1
+sh random_reason.sh Qwen2.5-3B-GRPO-800 ProofWriter newdirectbase
+sh random_reason.sh Qwen2.5-3B-GRPO-800 LOGIQA newdirectbase
+sh random_reason.sh Qwen2.5-3B-GRPO-800 FOLIO newdirectbase
+python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path ./exp_cot/output/output.MATH500.newdirectbase.math_teacher.Qwen2.5-3B-GRPO-800.json --mode 1
 # python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path exp_cot/output/output.MATH500.newdirectbase.randomreason.Qwen2.5-3B-SFT-GRPO-2000.modedefault.json --mode 0
 # python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path exp_cot/output/output.MATH500.newdirectbase.defaultreason.Qwen2.5-3B-SFT-GRPO-2000.modedefault.json --mode 0
 # python ./scripts/openai_math500.py --model_name gpt-4.1-mini --file_path exp_cot/output/output.MATH500.newdirectbase.defaultreason_strongbias.Qwen2.5-3B-SFT-GRPO-2000.modedefault.json --mode 0
