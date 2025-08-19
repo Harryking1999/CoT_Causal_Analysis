69e42a7724ac602b796e0bdb2903f2eb5300e647
Harryking1999 <fuzhizhang.fzz@gmail.com>
2025-08-19 19:28:37 +0800

    add file

--- test_GSM_noop.sh
+++ test_GSM_noop.sh
@@ -1,5 +1,5 @@
 python test_GSM_noop.py \
-    --api_base "http://localhost:8080/v1" \
+    --api_base "http://localhost:8081/v1" \
     --model_name "Qwen2.5-3B-Distill-22000" \
     --max_tokens 20000 \
     --seed 42 \
